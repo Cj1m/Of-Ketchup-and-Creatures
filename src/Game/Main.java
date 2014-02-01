115742bf7f78415820022d5843c2849fe97ffc92
cj1m <cj1mckay@gmail.com>
2014-02-01 16:37:19 +0000

    Introducing Options Class From JNL

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -17,13 +17,15 @@ public class Main {
 	String bill="Traveller Bill";
 	
 	String option;
-	String lastwords = "'I didn't mean no harm...'";
+	
+	int rn;
+	int rn2;
+	int rn3;
 	
 	int handOff;
-	
 	int health = 10;
 	
-	String[] foods = {"Potatos", "Carrots", "Beans", "Marmite", "Ketchup", "Beef","Pork", "Kentucky Fried Curry", "Dirt"};
+	String[] foods = {"Potatos", "Carrots", "Beans", "Marmite", "Ketchup", "Beef","Pork", "Kentucky Fried Curry"};
 	ArrayList<String> inv = new ArrayList<String>();
 	
 	public static void main(String[] args) {
@@ -34,7 +36,7 @@ public class Main {
 		game();
 	}
 	
-	private void game() {
+	public void game() {
 		welcome();
 		try {
 			Thread.sleep(5000);
@@ -45,7 +47,7 @@ public class Main {
 		part1();
 	}
 	
-	private void welcome(){
+	public void welcome(){
 		Say("Welcome to 'Of Ketchup and Creatures', Enjoy your playthrough...");
 		Say("----------------------------------------------------------------");
 		Say("What's your name, adventurer?");
@@ -65,7 +67,7 @@ public class Main {
 	
 	//Part 1
 	//Part 1
-	private void part1(){
+	public void part1(){
 		Say("------------------------------------");
 		Say("CHAPTER UNO: GOODBYE MY DEAR KETCHUP");
 		Say("------------------------------------");
@@ -85,28 +87,14 @@ public class Main {
 		option = sc.nextLine();
 		option = option.toLowerCase();
 		
-		if(option.equals("a")){
-			part1Death();
-		}else if(option.equals("b")){
-			Say("You turn back, but by the time you arrive at the Town, you have forgotten why you are there. #LazyDeveloper");
-			part1();
-		}else if(option.equals("c")){
-			Say("You accept that you will not be having ketchup with your beans tonight and carry on with your adventure.");
-			part2();
-		}else{
-			error(1);
-		}
-		
+		@SuppressWarnings("unused")
+		Options chapter1Options = new Options(usr, option, 1, this);
 	}
 	
-	private void part1Death(){
-		Say(gnome +" shouts 'DIOS ES GRANDE!', ignites his lighter, and sets the wagon on fire. Your last words were " + lastwords);
-		game();
-	}
 	
 	//Part 2
 	//Part 2
-	private void part2(){
+	public void part2(){
 		Say("---------------------");
 		Say("CHAPTER DOS: HAND OFF");
 		Say("---------------------");
@@ -136,41 +124,14 @@ public class Main {
 		}
 		Say("CHOMP!");
 		
-		if(option.equals(gnome.toLowerCase())){
-			Say(gnome + " screams with rage and runs off into the distance with a trail of fire behind him. " + gnome + " has left the party, nice one...");
-			handOff = 1;
-		}else if(option.equals(orc.toLowerCase())){
-			health--;
-			Say(orc + " looks at his hand, then back at you - his face full of rage. He takes a swing at your head. Your health is now: " + health);
-			handOff = 2;
-		}else if(option.equals(wizard.toLowerCase())){
-			Say(wizard + " waves his wand over his wrist. Instead of growing a hand, he grows a flower. " + wizard + " is angry.");
-			handOff = 3;
-		}else{
-			error(2);
-		}
 		
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		Say("");
-		Say("A bright light catches your eye. You look into the distance and see a brightly lit market - a food market.");
-		
-		if(handOff == 2){
-			health--;
-			Say(orc + " looks at his hand again, then back at you - his face filling with more rage.");
-		    Say(" He wraps his hands around his club and takes yet another swing at your head. Your health is now: " + health);
-		}
-		part3();   
+		@SuppressWarnings("unused")
+		Options chapter2Options = new Options(usr, option, 2, this);  
 	}
 	
 	//Part 3
 	//Part 3
-	private void part3(){
+	public void part3(){
 		Say("----------------------------");
 		Say("CHAPTER TRES: TRAVELLER BILL");
 		Say("----------------------------");
@@ -183,79 +144,14 @@ public class Main {
 		option = sc.nextLine();
 		option = option.toLowerCase();
 		
-		if(option.equals("smack him in the face") || option.equals("punch him in the face") || option.equals("sing a justin beiber song") || option.equals("twerk") || option.equals("dios es grande")){
-			part3Death();	
-		}else if(option.equals("help me") || option.equals("come here") || option.equals("yes") || option.equals("talk to me") || option.equals("talk to me or i'll bash your skull in") || option.equals("ask for help")){
-			Say(bill + " Smiles and asks you where you're headed. " + dwarf + " replies, 'We are venturing to the Foggy Mountains (or Hills), could you give us directions?'");
-			try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			Say(bill + " nods, 'Yes, I can give you directions - but only if I am able to be apart of this adventure.'");
-			
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			Say("Talk...");
-			Say("");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			Say("Argue..");
-			Say("");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			Say("Conclude.");
-			Say("");
-			
-			Say("After a very 'lively' conversation with your party (at one point " + orc + " threatend to bash his own skull in!), you conclude that " + bill + " is allowed to join the party.");
-			Say("");
-			Say("'Now' says " + bill + ", 'time to get some supplies..'");
-			Say("");
-			
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			Say("");
-			Say("2 Hours Later..");
-			Say("");
-			
-			part3AndAHalf();
-		}else{
-			error(3);
-		}
+		@SuppressWarnings("unused")
+		Options chapter3Options = new Options(usr, option, 3, this);
 	}
 	
 	
-	private void part3Death(){
-		Say("");
-		Say("The man is very insulted and shoots you in the head. BOOM-- Headshot.");
-		Say("---------------------------------------------------------------------");
-		health -= 10;;
-		Say("YOU'RE HEALTH IS NOW " + health + ". YOU ARE DEAD!");
-		Say("");
-		health = 10;
-		game();
-	}
-	
 	//Part 3.5
 	//Part 3.5
-	private void part3AndAHalf() {
+	public void part3AndAHalf() {
 		Say("You arrive at the market exhausted after your long hike. You're more hungry than ever and see a food stall feet away.");
 		Say("When you arrive a upbeat man welcomes you to his stall. He says:");
 		Say("'Welcome fellow travellers, my name is Frank, what can I do you for? We have a large range of foods, can I interest you in any?");
@@ -265,11 +161,11 @@ public class Main {
 		Say("");
 		
 		Say("I've got: ");
-		int rn = (int) Math.round(Math.random() * 8);
+		rn = (int) Math.round(Math.random() * 8);
 		Say(foods[rn]);
-		int rn2 = (int) Math.round(Math.random() * 8);
+		rn2 = (int) Math.round(Math.random() * 8);
 		Say(foods[rn2]);
-		int rn3 = (int) Math.round(Math.random() * 8);
+		rn3 = (int) Math.round(Math.random() * 8);
 		Say(foods[rn3]);
 		Say("");
 		
@@ -277,41 +173,19 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		option = sc.nextLine();
 		option = option.toLowerCase();
-		if(option.equals(foods[rn].toLowerCase())){
-			Say("Frank says 'Thank you, come again' in a strange and foriegn accent.");
-			inv.add(foods[rn]);
-		}else if(option.equals(foods[rn2].toLowerCase())){
-			Say("Frank says 'Thank you, sir!'");
-			inv.add(foods[rn2]);
-		}else if(option.equals(foods[rn3].toLowerCase())){
-			Say("Frank says 'Excellent choice!'");
-			inv.add(foods[rn3]);
-		}
-		
-		Say("You thank Frank for his generosity, and you continue on your quest...");
-		Say("NEW ITEM AQUIRED: " + inv.get(0));
-		
-		part4();
+		@SuppressWarnings("unused")
+		Options chapter305Options = new Options(usr, option, 305, this);
 	}
 	
 	//Part 4
 	//Part 4
-	private void part4(){
+	public void part4(){
 		/* Part 4:
 		*   Far over the Foggy Mountain or Hill
 		*   To dungeons deep with our traveller Bill
 		*   We could away ere break of day
 		*   To find our long-forgotten ASPARAGUS!
 		*/
-	}
-	
-	private void error(int i){
-		Say("Are you mad?");
-		if(i == 1)part1();
-		if(i == 2)part2();
-		if(i == 3)part3();
-		if(i == 304)part3AndAHalf();
-		if(i == 4)part4();Say("Sorry, that desicion is not valid");	//ATM 	
 	}
 	
 	public void Say(String x){
---
+++ src/Game/Options.java
@@ -0,0 +1,179 @@
+package Game;
+
+public class Options {
+	String usr;
+	String dwarf ="syrupsteam";
+	String orc="jeffrey";
+	String gnome="vietgnome";
+	String wizard="happy potter";
+	String bill="traveller bill";
+	
+	String lastwords = "'I didn't mean no harm...'";
+	
+	Main game;
+	
+	public Options(String usr, String option, int chapter, Main main){
+		game = main;
+		this.usr = usr;
+		if(chapter == 1)o1(option);
+		if(chapter == 2)o2(option);
+		if(chapter == 3)o3(option);
+		if(chapter == 305)o3h(option);
+	}
+	private void o1(String option){
+		if(option.equals("a")){
+			part1Death();
+		}else if(option.equals("b")){
+			Say("You turn back, but by the time you arrive at the Town, you have forgotten why you are there. #LazyDeveloper");
+			game.part1();
+		}else if(option.equals("c")){
+			Say("You accept that you will not be having ketchup with your beans tonight and carry on with your adventure.");
+			game.part2();
+		}else{
+			error(1);
+		}
+	}
+	private void o2(String option){
+		if(option.equals(gnome)){
+			Say(gnome + " screams with rage and runs off into the distance with a trail of fire behind him. " + gnome + " has left the party, nice one...");
+			game.handOff = 1;
+		}else if(option.equals(orc)){
+			game.health--;
+			Say(orc + " looks at his hand, then back at you - his face full of rage. He takes a swing at your head. Your health is now: " + game.health);
+			game.handOff = 2;
+		}else if(option.equals(wizard)){
+			Say(wizard + " waves his wand over his wrist. Instead of growing a hand, he grows a flower. " + wizard + " is angry.");
+			game.handOff = 3;
+		}else{
+			error(2);
+		}
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Say("");
+		Say("A bright light catches your eye. You look into the distance and see a brightly lit market - a food market.");
+		
+		if(game.handOff == 2){
+			game.health--;
+			Say(orc + " looks at his hand again, then back at you - his face filling with more rage.");
+		    Say(" He wraps his hands around his club and takes yet another swing at your head. Your health is now: " + game.health);
+		}
+		game.part3();
+	}	
+	private void o3(String option){
+		if(option.equals("smack him in the face") || option.equals("punch him in the face") || option.equals("sing a justin beiber song") || option.equals("twerk") || option.equals("dios es grande")){
+			part3Death();	
+		}else if(option.equals("help me") || option.equals("come here") || option.equals("yes") || option.equals("talk to me") || option.equals("talk to me or i'll bash your skull in") || option.equals("ask for help")){
+			Say(bill + " Smiles and asks you where you're headed. " + dwarf + " replies, 'We are venturing to the Foggy Mountains (or Hills), could you give us directions?'");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Say(bill + " nods, 'Yes, I can give you directions - but only if I am able to be apart of this adventure.'");
+			
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			Say("Talk...");
+			Say("");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Say("Argue..");
+			Say("");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			Say("Conclude.");
+			Say("");
+			
+			Say("After a very 'lively' conversation with your party (at one point " + orc + " threatend to bash his own skull in!), you conclude that " + bill + " is allowed to join the party.");
+			Say("");
+			Say("'Now' says " + bill + ", 'time to get some supplies..'");
+			Say("");
+			
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			Say("");
+			Say("2 Hours Later..");
+			Say("");
+			
+			game.part3AndAHalf();
+		}else{
+			error(3);
+		}
+	}
+	private void o3h(String option){
+		if(option.equals(game.foods[game.rn].toLowerCase())){
+			Say("Frank says 'Thank you, come again' in a strange and foriegn accent.");
+			game.inv.add(game.foods[game.rn]);
+		}else if(option.equals(game.foods[game.rn2].toLowerCase())){
+			Say("Frank says 'Thank you, sir!'");
+			game.inv.add(game.foods[game.rn2]);
+		}else if(option.equals(game.foods[game.rn3].toLowerCase())){
+			Say("Frank says 'Excellent choice!'");
+			game.inv.add(game.foods[game.rn3]);
+		}
+		
+		Say("You thank Frank for his generosity, and you continue on your quest...");
+		Say("NEW ITEM AQUIRED: " + game.inv.get(0));
+		
+		game.part4();
+	}
+	
+	//Unused because Chapter 4 is not done 
+	@SuppressWarnings("unused")
+	private void o4(String option){
+	
+	}
+	
+	
+	//Deaths and Error
+	private void part1Death(){
+		Say(gnome +" shouts 'DIOS ES GRANDE!', ignites his lighter, and sets the wagon on fire. Your last words were " + lastwords);
+		game.game();
+	}
+	
+	private void part3Death(){
+		Say("");
+		Say("The man is very insulted and shoots you in the head. BOOM-- Headshot.");
+		Say("---------------------------------------------------------------------");
+		game.health -= 10;;
+		Say("YOU'RE HEALTH IS NOW " + game.health + ". YOU ARE DEAD!");
+		Say("");
+		game.health = 10;
+		game.game();
+	}
+	
+	private void error(int i){
+		Say("Are you mad?");
+		if(i == 1)game.part1();
+		if(i == 2)game.part2();
+		if(i == 3)game.part3();
+		if(i == 304)game.part3AndAHalf();
+		if(i == 4)game.part4();Say("Sorry, that desicion is not valid");	//ATM 	
+	}
+	public void Say(String x){
+		System.out.println(x);
+	}
+}
